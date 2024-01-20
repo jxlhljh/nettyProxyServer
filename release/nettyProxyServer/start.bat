fbb0b418b994725221dc1baeb28bb32f73e5212f
liujh <liujh@gzsendi.cn>
2024-01-20 20:32:30 +0800

    init

--- release/nettyProxyServer/start.bat
+++
@@ -1,3 +0,0 @@
-title "nettyProxyServerStart"
-
-java -server -Xmx256m -Xms256m -Xmn128m -cp ".;./nettyProxyServer.jar;./lib/*;" cn.lihua.MainServerStart
--- release/nettyProxyServer/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# enter script directory
-script_directory=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-cd $script_directory
-
-java -server -Xmx256m -Xms256m -Xmn128m -cp ".:./nettyProxyServer.jar:./lib/*:" cn.lihua.MainServerStart &
